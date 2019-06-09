7a06fb9f3ea77686f255588d03868878ac69357a
Aiden <aiden.egf.arnold@gmail.com>
2019-06-08 18:28:58 -0700

    working model at 67.5% acc

---
+++ scripts/video.py
@@ -0,0 +1,62 @@
+import cv2
+from fastai import *
+from fastai.vision import *
+import PIL
+
+# load fast.ai learner object
+learn = load_learner('../models/')
+
+
+def preprocess_input(x):
+	"""A function to apply preprocessing to the detected face
+	for input to the fast.ai classifier"""
+
+# load classifier
+casc_path = '../haar/haarcascade_frontalface_default.xml'
+face_casc = cv2.CascadeClassifier(casc_path)
+
+# get webcam
+video_capture = cv2.VideoCapture(0)
+
+while True:
+	# capture frame-by-frame
+	frame = video_capture.read()[1]
+	# convert to grayscale
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# detect faces
+	faces = face_casc.detectMultiScale(gray,
+									   scaleFactor=1.1,
+									   minNeighbors=5,
+									   minSize=(30, 30),
+									   flags=cv2.CASCADE_SCALE_IMAGE
+									   )
+
+
+	for (x, y, w, h) in faces:
+		# draw a rectangle around the faces
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		# extract face
+		gray = gray[y:y+h, x:x+w]
+		# convert from array to image
+		img = PIL.Image.fromarray(gray, 'L')
+		# get emotion prediction
+		try: 
+			# convert to fastai Image object
+			img = Image(pil2tensor(img, dtype=np.float32).div_(255))
+			pred = learn.predict(img)
+			# set probability threshold
+			if pred[2][int(pred[1])] > .3:
+				emotion = pred[0]
+			cv2.putText(frame, str(emotion), (int(x+w/3), y+h+25), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,0))
+		except: pass
+
+		
+		# Display the resulting frame
+		cv2.imshow('Video', frame)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
